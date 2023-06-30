204927903ee04334d8a64194fb9383e3111ce817
dataPerceptron <122875409+dataPerceptron@users.noreply.github.com>
2023-06-30 15:21:35 +0100

    First commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Amplify } from "aws-amplify";
+import awsExports from "./src/aws-exports";
+Amplify.configure(awsExports);
+
+import { withAuthenticator } from "@aws-amplify/ui-react-native";
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text> MIA Todays News </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default withAuthenticator(App);
+// export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "miaamplifye5a3e849": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}

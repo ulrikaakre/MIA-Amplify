98ebb6dd2df8fefdc96d4b5492f5075af5745256
ulrikaakre <aakreulrik@gmail.com>
2023-08-24 16:24:46 +0100

    recreating API

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-import { Amplify, Auth } from "aws-amplify";
+import { API, Amplify, graphqlOperation } from "aws-amplify";
 import awsExports from "./src/aws-exports";
 Amplify.configure(awsExports);
 
-// graphqlOperation
-
 import { withAuthenticator } from "@aws-amplify/ui-react-native";
+
+import { listTextPosts, listVideoPosts } from "./src/graphql/queries";
 
 const App = () => {
   const [text, onChangeText] = useState("");
   const [title, onChangeTitle] = useState("");
+
+  const [textPosts, setTextPosts] = useState([]);
+  const [videoPosts, setVideoPosts] = useState([]);
 
   // const addPost = () => {
   //   const { title, content } = textPostData;
@@ -23,19 +26,76 @@ const App = () => {
   // };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      const userInfo = await Auth.currentAuthenticatedUser({
-        bypassCache: true,
-      });
-      console.log("userinfo: " + userInfo);
+    // const fetchUser = async () => {
+    //   const userInfo = await Auth.currentAuthenticatedUser({
+    //     bypassCache: true,
+    //   });
+    //   console.log("attributes: ", userInfo.attributes);
+    //   console.log("sub: ", userInfo.attributes.sub);
+    //   if (!userInfo) {
+    //     return;
+    //   }
+
+    //   try {
+    //     const getUserResponse = await API.graphql({
+    //       query: getUser,
+    //       variables: { id: userInfo.attributes.sub.toString() },
+    //       authMode: "AMAZON_COGNITO_USER_POOLS",
+    //     });
+
+    //     if (getUserResponse.data && getUserResponse.data.getUser) {
+    //       console.log("User exists.");
+    //     }
+    //   } catch (error) {
+    //     console.error("Error fetching user:", error);
+    //   }
+
+    //   const newUser = {
+    //     id: userInfo.attributes.sub,
+    //     username: userInfo.username,
+    //     email: userInfo.attributes.email,
+    //   };
+
+    //   try {
+    //     await API.graphql({
+    //       query: getUser,
+    //       variables: { newUser },
+    //       authMode: "AMAZON_COGNITO_USER_POOLS",
+    //     });
+    //   } catch (error) {
+    //     console.error("Error creating user:", error);
+    //   }
+    // };
+    // fetchUser();
+
+    const fetchTextPost = async () => {
+      try {
+        const res = await API.graphql(graphqlOperation(listTextPosts));
+        console.log(res);
+        setTextPosts(res.data.listTextPosts.items);
+        console.log("textPosts", textPosts.user.username);
+      } catch (err) {
+        console.log("Text Post fetch erros: ", err);
+      }
     };
-    fetchUser;
+    const fetchVideoPost = async () => {
+      try {
+        const res = await API.graphql(graphqlOperation(listVideoPosts));
+        console.log(res);
+        setVideoPosts(res.data.listVideoPosts.items);
+        console.log("videoPosts", videoPosts.user.username);
+      } catch (err) {
+        console.log("Video Post fetch erros: ", err);
+      }
+    };
+    fetchTextPost();
+    fetchVideoPost();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={{ margin: 20 }}> MIA Todays News!! </Text>
-      <TextInput
+      <Text style={{ margin: 20, fontSize: 32 }}> MIA Todays News! </Text>
+      {/* <TextInput
         style={{ borderWidth: 1, padding: 5, margin: 5, width: 150 }}
         placeholder="Title"
         onChangeText={onChangeTitle}
@@ -53,8 +113,42 @@ const App = () => {
         onChangeText={onChangeText}
         value={text}
       />
-      {/* <Button title="Publish" onPress={addPost} /> */}
-      <Button title="Publish" />
+      <Button title="Publish" /> */}
+      {textPosts.map((e) => (
+        <View
+          key={e.id}
+          style={{
+            marginBottom: 10,
+            padding: 10,
+            backgroundColor: "lightgray",
+            width: 350,
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ fontSize: 23 }}>{e.title}</Text>
+          {e.user && (
+            <Text style={{ fontSize: 16, fontStyle: "italic" }}>
+              Username: {e.user.username}
+            </Text>
+          )}
+          <Text style={{ fontSize: 18, marginTop: 10 }}>{e.content}</Text>
+        </View>
+      ))}
+
+      {/* {videoPosts.map((video) => (
+        <View
+          key={video.id}
+          style={{
+            marginBottom: 10,
+            padding: 10,
+            backgroundColor: "lightgray",
+          }}
+        >
+          <Text style={{ fontSize: 23 }}>Video Title</Text>
+          <Video source={video.videoURI} paused={false} />
+        </View>
+      ))} */}
+
       <StatusBar style="auto" />
     </View>
   );
